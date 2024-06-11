6049eb8214dd01e9dccba52acdc76dd9a1acfe17
Giulio Ermanno Pibiri <jeis90@gmail.com>
2024-06-11 15:50:30 +0200

    comment

--- include/index.hpp
+++ include/index.hpp
@@ -35,8 +35,11 @@ struct index {
     }
 
     void print_stats() const;
-    void dump_colors(std::ofstream& os) const;
     void dump(std::string const& basename) const;
+
+    /* this dumps the colors in a custom format: useful for internal analysis,
+       but commented out for the public API */
+    // void dump_colors(std::ofstream& os) const;
 
     uint64_t k() const { return m_k2u.k(); }
     uint64_t num_docs() const { return m_ccs.num_docs(); }

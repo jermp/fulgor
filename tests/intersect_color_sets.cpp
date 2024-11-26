26f85950fb7ce53b8bdb695c3c742ff656877fcf
Alessio Campanelli <ale.campa00@gmail.com>
2024-11-26 11:09:26 +0000

    Density-based intersection test

--- tests/intersect_color_sets.cpp
+++ tests/intersect_color_sets.cpp
@@ -17,39 +17,62 @@ int intersect_color_sets(std::string const& index_filename, uint64_t size, std::
     essentials::load(index, index_filename.c_str());
     if (!quiet) essentials::logger("DONE");
 
-    // pseudoalignment_algorithm algo = pseudoalignment_algorithm::FULL_INTERSECTION;
+    uint32_t num_intersections = 1000000;
+    double density_threshold = 0.25;
+    uint32_t n = size;
+    uint32_t num_colors = index.num_colors();
+    srand(42);
 
     essentials::timer<std::chrono::high_resolution_clock, std::chrono::milliseconds> t;
-    t.start();
 
-    uint32_t num_intersections = 1000000;
+    std::vector<std::vector<uint32_t>> color_sets_by_density(std::ceil(1/density_threshold));
 
-    srand(42);
-    uint32_t n = size;
-
-    for (uint32_t i = 0; i < num_intersections; i++) {
-        vector<typename FulgorIndex::color_sets_type::iterator_type> iterators;
-        for (uint32_t j = 0; j < n; j++) {
-            iterators.push_back(index.color_set(rand() % index.num_color_sets()));
-        }
-        vector<uint32_t> colors;
-        if (algo == "geq") {
-            next_geq_intersect(iterators, colors, index.num_colors());
-        } else {
-            counting_intersect(iterators, colors, index.num_colors());
-        }
+    for(uint32_t color_set_id = 0; color_set_id < index.num_color_sets(); ++color_set_id){
+        auto it = index.color_set(color_set_id);
+        double density = 1.0 * it.size() / num_colors;
+        color_sets_by_density[std::ceil(density/density_threshold)-1].push_back(color_set_id);  // std::floor throws segfault if density == 1
     }
 
-    t.stop();
+    for (uint32_t density_group = 0; density_group < color_sets_by_density.size(); density_group++){
+        std::cout << color_sets_by_density[density_group].size() << " ";
+    }
+    std::cout << endl;
+
+    for (uint32_t density_group = 0; density_group < color_sets_by_density.size(); density_group++){
+        uint16_t group_size = color_sets_by_density[density_group].size();
+        t.start();
+        for (uint32_t i = 0; i < num_intersections; i++) {
+            vector<typename FulgorIndex::color_sets_type::iterator_type> iterators;
+            for (uint32_t j = 0; j < n; j++) {
+                uint32_t id = color_sets_by_density[density_group][rand() % group_size];
+                iterators.push_back(index.color_set(id));
+            }
+            vector<uint32_t> colors;
+
+            if (algo == "geq") {
+                next_geq_intersect(iterators, colors, index.num_colors());
+            } else {
+                counting_intersect(iterators, colors, index.num_colors());
+            }
+        }
+        t.stop();
+
+        if (!quiet) {
+            std::cout << "intersected " << num_intersections * size << " color_sets with density [" 
+                      << density_threshold*density_group << ", " << density_threshold*(density_group + 1) 
+                      << "]" << std::endl;
+            std::cout << "elapsed = " << t.elapsed() << " millisec / "
+                      << t.elapsed() / 1000 << " sec / "
+                      << t.elapsed() / 1000 / 60 << " min / "
+                      << (t.elapsed() * 1000) / num_intersections << " musec/intersection" << std::endl;
+        }
+        t.reset();
+    }
+
+
     if (!quiet) essentials::logger("DONE");
 
-    if (!quiet) {
-        std::cout << "intersected " << num_intersections << " reads" << std::endl;
-        std::cout << "elapsed = " << t.elapsed() << " millisec / ";
-        std::cout << t.elapsed() / 1000 << " sec / ";
-        std::cout << t.elapsed() / 1000 / 60 << " min / ";
-        std::cout << (t.elapsed() * 1000) / num_intersections << " musec/intersection" << std::endl;
-    }
+    
 
     return 0;
 }

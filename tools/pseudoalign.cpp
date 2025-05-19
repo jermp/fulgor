69759dfa75f5b8289801e617789a29dc937e197d
Giulio Ermanno Pibiri <jeis90@gmail.com>
2025-05-19 16:41:13 +0200

    removed skipping heuristics from the main branch (see Releases)

--- tools/pseudoalign.cpp
+++ tools/pseudoalign.cpp
@@ -7,35 +7,12 @@
 #include "external/FQFeeder/include/FastxParser.hpp"
 #include "external/FQFeeder/src/FastxParser.cpp"
 
-#include "piscem_psa/hit_searcher.cpp"
-#include "kallisto_psa/psa.cpp"
-#include "src/psa/full_intersection.cpp"
-#include "src/psa/threshold_union.cpp"
+#include "src/ps_full_intersection.cpp"
+#include "src/ps_threshold_union.cpp"
 
 using namespace fulgor;
 
-// See https://github.com/jermp/experiments/tree/master/bin_to_char_conversion.
-// void write_output(std::vector<uint32_t> const& vec, std::ostream& os) {
-//     static char buffer[32];
-//     for (uint32_t x : vec) {
-//         int len = 1;
-//         do {
-//             buffer[len++] = '0' + (x % 10);
-//             x /= 10;
-//         } while (x > 0);
-//         std::reverse(buffer + 1, buffer + len);
-//         buffer[0] = '\t';
-//         os.write(buffer, len);
-//     }
-//     os << '\n';
-// }
-
-enum class pseudoalignment_algorithm : uint8_t {
-    FULL_INTERSECTION,
-    THRESHOLD_UNION,
-    SKIPPING,
-    SKIPPING_KALLISTO
-};
+enum class pseudoalignment_algorithm : uint8_t { FULL_INTERSECTION, THRESHOLD_UNION };
 
 std::string to_string(pseudoalignment_algorithm algo, double threshold) {
     std::string o;
@@ -46,12 +23,6 @@ std::string to_string(pseudoalignment_algorithm algo, double threshold) {
         case pseudoalignment_algorithm::THRESHOLD_UNION:
             o = "threshold-intersection (t = " + std::to_string(threshold) + ")";
             break;
-        case pseudoalignment_algorithm::SKIPPING:
-            o = "skipping-pseudoalignment";
-            break;
-        case pseudoalignment_algorithm::SKIPPING_KALLISTO:
-            o = "kallisto-pseudoalignment";
-            break;
     }
     return o;
 }
@@ -60,127 +31,50 @@ template <typename FulgorIndex>
 int do_map(FulgorIndex const& index, fastx_parser::FastxParser<fastx_parser::ReadSeq>& rparser,
            std::atomic<uint64_t>& num_reads, std::atomic<uint64_t>& num_mapped_reads,
            pseudoalignment_algorithm algo, const double threshold, std::ofstream& out_file,
-           std::mutex& iomut, std::mutex& ofile_mut) {
+           std::mutex& iomut, std::mutex& ofile_mut)  //
+{
     std::vector<uint32_t> colors;  // result of pseudo-alignment
     std::stringstream ss;
     uint64_t buff_size = 0;
     constexpr uint64_t buff_thresh = 50;
 
-    if ((algo == pseudoalignment_algorithm::SKIPPING or
-         algo == pseudoalignment_algorithm::SKIPPING_KALLISTO) and
-        (index.get_k2u().canonicalized())) {
-        std::vector<uint64_t> unitig_ids;                           // for use with skipping
-        std::vector<std::pair<projected_hits, int>> kallisto_hits;  // for use with kallisto psa
-
-        piscem_psa::hit_searcher<FulgorIndex> hs(&index);
-        sshash::streaming_query_canonical_parsing qc(&index.get_k2u());
-
-        auto get_hits_piscem_psa = [&qc, &hs](const std::string& seq,
-                                              std::vector<uint64_t>& unitig_ids) -> void {
-            hs.clear();
-            auto had_hits = hs.get_raw_hits_sketch(seq, qc, true, false);
-            if (had_hits) {
-                for (auto& h : hs.get_left_hits()) {
-                    if (!h.second.empty()) { unitig_ids.push_back(h.second.contigIdx_); }
-                }
+    auto rg = rparser.getReadGroup();
+    while (rparser.refill(rg)) {
+        for (auto const& record : rg) {
+            switch (algo) {
+                case pseudoalignment_algorithm::FULL_INTERSECTION:
+                    index.pseudoalign_full_intersection(record.seq, colors);
+                    break;
+                case pseudoalignment_algorithm::THRESHOLD_UNION:
+                    index.pseudoalign_threshold_union(record.seq, colors, threshold);
+                    break;
+                default:
+                    break;
             }
-        };
-
-        auto get_hits_kallisto_psa = [&index, &kallisto_hits](
-                                         const std::string& seq,
-                                         std::vector<uint64_t>& unitig_ids) -> void {
-            kallisto_hits.clear();
-            match(seq, seq.length(), &index, kallisto_hits);
-            if (!kallisto_hits.empty()) {
-                for (auto& h : kallisto_hits) { unitig_ids.push_back(h.first.contigIdx_); }
+            buff_size += 1;
+            if (!colors.empty()) {
+                num_mapped_reads += 1;
+                ss << record.name << '\t' << colors.size();
+                for (auto c : colors) { ss << "\t" << c; }
+                ss << '\n';
+                // write_output(colors, ss);
+            } else {
+                ss << record.name << "\t0\n";
             }
-        };
-        // Get the read group by which this thread will
-        // communicate with the parser (*once per-thread*)
-        auto rg = rparser.getReadGroup();
-        while (rparser.refill(rg)) {
-            // Here, rg will contain a chunk of read pairs we can process.
-            for (auto const& record : rg) {
-                switch (algo) {
-                    case pseudoalignment_algorithm::SKIPPING:
-                        get_hits_piscem_psa(record.seq, unitig_ids);
-                        break;
-                    case pseudoalignment_algorithm::SKIPPING_KALLISTO:
-                        get_hits_kallisto_psa(record.seq, unitig_ids);
-                        break;
-                    default:
-                        break;
-                }
-
-                num_reads += 1;
-                index.intersect_unitigs(unitig_ids, colors);
-                if (!colors.empty()) {
-                    num_mapped_reads += 1;
-                    ss << record.name << '\t' << colors.size() << '\t';
-                    for (auto c : colors) { ss << "\t" << c; }
-                    ss << '\n';
-                    // write_output(colors, ss);
-                } else {
-                    ss << record.name << "\t0\n";
-                }
-                colors.clear();
-                unitig_ids.clear();
-
-                buff_size += 1;
-                if (num_reads > 0 and num_reads % 1000000 == 0) {
-                    iomut.lock();
-                    std::cout << "mapped " << num_reads << " reads" << std::endl;
-                    iomut.unlock();
-                }
-                if (buff_size > buff_thresh) {
-                    std::string outs = ss.str();
-                    ss.str("");
-                    ofile_mut.lock();
-                    out_file.write(outs.data(), outs.size());
-                    ofile_mut.unlock();
-                    buff_size = 0;
-                }
+            num_reads += 1;
+            colors.clear();
+            if (num_reads > 0 and num_reads % 1000000 == 0) {
+                iomut.lock();
+                std::cout << "mapped " << num_reads << " reads" << std::endl;
+                iomut.unlock();
             }
-        }
-    } else {
-        auto rg = rparser.getReadGroup();
-        while (rparser.refill(rg)) {
-            for (auto const& record : rg) {
-                switch (algo) {
-                    case pseudoalignment_algorithm::FULL_INTERSECTION:
-                        index.pseudoalign_full_intersection(record.seq, colors);
-                        break;
-                    case pseudoalignment_algorithm::THRESHOLD_UNION:
-                        index.pseudoalign_threshold_union(record.seq, colors, threshold);
-                        break;
-                    default:
-                        break;
-                }
-                buff_size += 1;
-                if (!colors.empty()) {
-                    num_mapped_reads += 1;
-                    ss << record.name << '\t' << colors.size();
-                    for (auto c : colors) { ss << "\t" << c; }
-                    ss << '\n';
-                    // write_output(colors, ss);
-                } else {
-                    ss << record.name << "\t0\n";
-                }
-                num_reads += 1;
-                colors.clear();
-                if (num_reads > 0 and num_reads % 1000000 == 0) {
-                    iomut.lock();
-                    std::cout << "mapped " << num_reads << " reads" << std::endl;
-                    iomut.unlock();
-                }
-                if (buff_size > buff_thresh) {
-                    std::string outs = ss.str();
-                    ss.str("");
-                    ofile_mut.lock();
-                    out_file.write(outs.data(), outs.size());
-                    ofile_mut.unlock();
-                    buff_size = 0;
-                }
+            if (buff_size > buff_thresh) {
+                std::string outs = ss.str();
+                ss.str("");
+                ofile_mut.lock();
+                out_file.write(outs.data(), outs.size());
+                ofile_mut.unlock();
+                buff_size = 0;
             }
         }
     }
@@ -214,13 +108,6 @@ int pseudoalign(std::string const& index_filename, std::string const& query_file
     }
 
     std::cerr << "query mode : " << to_string(algo, threshold) << "\n";
-
-    if (((algo == pseudoalignment_algorithm::SKIPPING) or
-         (algo == pseudoalignment_algorithm::SKIPPING_KALLISTO)) and
-        !(index.get_k2u().canonicalized())) {
-        std::cerr << "==> Warning: skipping is only supported for canonicalized indexes. <=="
-                  << std::endl;
-    }
 
     std::ifstream is(query_filename.c_str());
     if (!is.good()) {
@@ -295,7 +182,7 @@ int pseudoalign(int argc, char** argv) {
     bool quiet = false;
 
     CLI::App app{"Perform (color-only) pseudoalignment to a Fulgor index."};
-    app.add_option("-i,--index", index_filename, "The Fulgor index filename,")
+    app.add_option("-i,--index", index_filename, "The Fulgor index filename.")
         ->required()
         ->check(CLI::ExistingFile);
     app.add_option("-q,--query", query_filename,
@@ -309,14 +196,6 @@ int pseudoalign(int argc, char** argv) {
     app.add_option("-t,--threads", num_threads, "Number of threads.")->default_val(1);
     app.add_option("--threshold", threshold, "Threshold for threshold_union algorithm.")
         ->check(CLI::Range(0.0, 1.0));
-    auto skip_opt = app.add_flag_callback(
-        "--skipping", [&algo]() { algo = pseudoalignment_algorithm::SKIPPING; },
-        "Enable the skipping heuristic in pseudoalignment.");
-    app.add_flag_callback(
-           "--skipping-kallisto",
-           [&algo]() { algo = pseudoalignment_algorithm::SKIPPING_KALLISTO; },
-           "Enable the kallisto skipping heuristic in pseudoalignment.")
-        ->excludes(skip_opt);
     app.add_flag("--quiet", quiet, "Quiet mode: do not print status messages to stdout.");
     CLI11_PARSE(app, argc, argv);
 
